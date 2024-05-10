ff2520cbb4faf34f8eaa328517b6d4904ccc44f4
Thiago Rodrigues <thiagorodrigues@uricer.edu.br>
2024-05-10 19:58:25 -0300

    refactor: move comcol-page-browse-by to correct place and fix navbar

--- src/themes/uri/app/navbar/navbar.component.ts
+++ src/themes/uri/app/navbar/navbar.component.ts
@@ -9,14 +9,11 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ContextHelpToggleComponent } from '@app/header/context-help-toggle/context-help-toggle.component';
 import { NavbarComponent as BaseComponent } from '@app/navbar/navbar.component';
-import { SearchNavbarComponent } from '@app/search-navbar/search-navbar.component';
 import { ThemedSearchNavbarComponent } from '@app/search-navbar/themed-search-navbar.component';
 import { slideMobileNav } from '@app/shared/animations/slide';
 import { ThemedAuthNavMenuComponent } from '@app/shared/auth-nav-menu/themed-auth-nav-menu.component';
 import { ThemedUserMenuComponent } from '@app/shared/auth-nav-menu/user-menu/themed-user-menu.component';
-import { UserMenuComponent } from '@app/shared/auth-nav-menu/user-menu/user-menu.component';
 import { ImpersonateNavbarComponent } from '@app/shared/impersonate-navbar/impersonate-navbar.component';
-import { LangSwitchComponent } from '@app/shared/lang-switch/lang-switch.component';
 import { ThemedLangSwitchComponent } from '@app/shared/lang-switch/themed-lang-switch.component';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
@@ -25,7 +22,7 @@ import { TranslateModule } from '@ngx-translate/core';
  * Component representing the public navbar
  */
 @Component({
-  selector: 'ds-navbar',
+  selector: 'ds-themed-navbar',
   styleUrls: ['./navbar.component.scss'],
   templateUrl: './navbar.component.html',
   animations: [slideMobileNav],
@@ -37,11 +34,8 @@ import { TranslateModule } from '@ngx-translate/core';
     NgClass,
     RouterLink,
     NgIf,
-    UserMenuComponent,
     NgFor,
     NgComponentOutlet,
-    SearchNavbarComponent,
-    LangSwitchComponent,
     ContextHelpToggleComponent,
     ThemedAuthNavMenuComponent,
     ImpersonateNavbarComponent,
